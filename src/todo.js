775ce674ab947cb3a4cfad3d70e7da0f3c573edc
lucaslija <81848948+lucaslija@users.noreply.github.com>
2024-06-13 18:01:03 -0700

    created todo.js

---
+++ src/todo.js
@@ -0,0 +1,8 @@
+function createTodo (name) {
+  let priority = false;
+  let description = "";
+  // integrate date functionality
+  let dueDate = ""; 
+
+  return {name, priority, description, dueDate};
+};
